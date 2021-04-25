aa2f2b611183281eb389ebec78650ae1ffd4b63b
Moses Ebenezer <moseschimdike@gmail.com>
2021-04-25 15:26:33 +0100

    auth checked

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
-import BaseController from './BaseController';
-// import { validationResult } from 'express-validator';
-// import {sequelize, db} from '../../models';
-
 import { matchedData } from 'express-validator/filter';
 import { validationResult } from 'express-validator/check';
+
+import BaseController from './BaseController';
 import { UserService } from '../services/user.service';
 import { UserInstance } from '../models/User';
 
@@ -19,7 +17,6 @@ class AuthenticationController extends BaseController {
 
 	static _register = async (req: Request, res: Response) => {
 		try {
-			// validate request body
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
 				return AuthenticationController._responseError(res, 'ECS004', 'Validation failed', errors.array(), 422);
@@ -38,14 +35,14 @@ class AuthenticationController extends BaseController {
 		try {
 			const errors = validationResult(req);
 
-			if (!errors.isEmpty()) return res.status(422).json(errors.array());
+			if (!errors.isEmpty()) {
+				return AuthenticationController._responseError(res, 'ECS004', 'Validation failed', errors.array(), 422);
+			}
 
 			const payload = matchedData(req) as UserInstance;
 			const token = userService.login(payload);
 
 			return token.then((t) => res.json(t));
-
-			return AuthenticationController._responseSuccess(res, '00', 'Successfully Fetched', null, 200);
 		} catch (error) {
 			return AuthenticationController._responseError(res, 'ECS005', 'An Error Occured', error, 500);
 		}
